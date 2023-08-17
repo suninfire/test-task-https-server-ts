09562bf53ee3c3641520b52fb53e0c1343cf53f0
Natali <n.suninfire@gmail.com>
2023-08-17 18:02:58 +0300

    qq

--- app.ts
+++ app.ts
@@ -5,27 +5,27 @@
 // };
 // console.log(user.name);
 
-interface IShapeAction {
-    area: () => number;
-    perimeter: () => number;
-}
-
-class Rectangle implements IShapeAction{
-
-    constructor(private a:number,private b: number) {
-
-    }
-
-    area(): number {
-        return this.a * this.b;
-    }
-
-    perimeter(): number {
-        return (this.a * 2)+(this.b * 2);
-    }
-
-}
-
-const rectangle = new Rectangle(10,12);
-console.log(rectangle.area());
-console.log(rectangle.perimeter());
+// interface IShapeAction {
+//     area: () => number;
+//     perimeter: () => number;
+// }
+//
+// class Rectangle implements IShapeAction{
+//
+//     constructor(private a:number,private b: number) {
+//
+//     }
+//
+//     area(): number {
+//         return this.a * this.b;
+//     }
+//
+//     perimeter(): number {
+//         return (this.a * 2)+(this.b * 2);
+//     }
+//
+// }
+//
+// const rectangle = new Rectangle(10,12);
+// console.log(rectangle.area());
+// console.log(rectangle.perimeter());
